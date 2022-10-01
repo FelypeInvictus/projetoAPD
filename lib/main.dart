ad659f7690d97aa14f3b0a55e35d2a3ad36a3e8d
Felype <felype.sales@outlook.com>
2022-09-30 22:36:24 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+import 'dart:ui';
+
+import 'package:app/rootpage.dart';
+import 'package:app/screens/transition/transicao.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+/*void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}*/
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = await SharedPreferences.getInstance();
+  final mostraTelaPrincipal = prefs.getBool('mostraTelaPrincipal') ?? false;
+
+  runApp(MeuApp(mostraTelaPrincipal: mostraTelaPrincipal));
+}
+
+//void main() => runApp(const MeuApp());
+
+class MeuApp extends StatelessWidget {
+  final bool mostraTelaPrincipal;
+
+  const MeuApp({
+    Key? key,
+    required this.mostraTelaPrincipal,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: const TextTheme(
+            bodyText2: TextStyle(fontSize: 64, color: Colors.lightGreen)),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.teal.shade700,
+            textStyle: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      //Só mude se souber o que está fazendo
+      home: mostraTelaPrincipal ? const RootPage() : const MyApp(),
+    );
+  }
+}
+
+
+// class MeuApp extends StatelessWidget {
+  
+//   const MeuApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(primarySwatch: Colors.green),
+//       home: const MyApp(),
+//     );
+//   }
+// }
