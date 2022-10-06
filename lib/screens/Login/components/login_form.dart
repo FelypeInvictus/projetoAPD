c7ab59661b4dad175d8ef541332ffc616cb89f1d
ALUNO CETEC <felype.sales@outlook.com>
2022-10-06 16:50:27 -0300

    Atualizado tela login

--- lib/screens/Login/components/login_form.dart
+++ lib/screens/Login/components/login_form.dart
@@ -17,87 +17,88 @@ class LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,  
-      child: Column(
-        children: [
-          TextFormField(
-            //Aqui é onde a entrada é recebida na variavel
-            onChanged: (inputReb) {
-              email = inputReb;
-            },
-            keyboardType: TextInputType.emailAddress,
-            textInputAction: TextInputAction.next,
-            cursorColor: kPrimaryColor,
-            onSaved: (email) {},
-            decoration: const InputDecoration(
-              hintText: "Seu e-mail",
-              prefixIcon: Padding(
-                padding: EdgeInsets.all(defaultPadding),
-                child: Icon(Icons.person),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-            child: TextFormField(
-              //Aqui é onde a entrada é recebida na variavel
-              onChanged: (inputReb) {
-                pwd = inputReb;
-              },
-              textInputAction: TextInputAction.done,
-              obscureText: true,
-              cursorColor: kPrimaryColor,
-              decoration: const InputDecoration(
-                hintText: "Sua senha",
-                prefixIcon: Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                  child: Icon(Icons.lock),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: defaultPadding),
-          Hero(
-            tag: "login_btn",
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return const RootPage();
-                    },
+    return Form(
+        child: SingleChildScrollView(
+            child: SizedBox(
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextFormField(
+                //Aqui é onde a entrada é recebida na variavel
+                onChanged: (inputReb) {
+                  email = inputReb;
+                },
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                cursorColor: kPrimaryColor,
+                onSaved: (email) {},
+                decoration: const InputDecoration(
+                  hintText: "Seu e-mail",
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                    child: Icon(Icons.person),
                   ),
-                );
-              },
-              // onPressed: () {  },
-
-              child: Text(
-                "Entrar".toUpperCase(),
-              ),
-            ),
-          ),
-          const SizedBox(height: defaultPadding),
-          AlreadyHaveAnAccountCheck(
-            press: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return const SignUpScreen();
-                  },
                 ),
-              );
-            },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+                child: TextFormField(
+                  //Aqui é onde a entrada é recebida na variavel
+                  onChanged: (inputReb) {
+                    pwd = inputReb;
+                  },
+                  textInputAction: TextInputAction.done,
+                  obscureText: true,
+                  cursorColor: kPrimaryColor,
+                  decoration: const InputDecoration(
+                    hintText: "Sua senha",
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(defaultPadding),
+                      child: Icon(Icons.lock),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: defaultPadding),
+              Hero(
+                tag: "login_btn",
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const RootPage();
+                        },
+                      ),
+                    );
+                  },
+                  // onPressed: () {  },
+
+                  child: Text(
+                    "Entrar".toUpperCase(),
+                  ),
+                ),
+              ),
+              const SizedBox(height: defaultPadding),
+              AlreadyHaveAnAccountCheck(
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return const SignUpScreen();
+                      },
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
-        ],
+        ),
       ),
-    ),
-    ),
-    );
+    )));
   }
 }
