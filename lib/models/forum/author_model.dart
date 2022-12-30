316d756b2c3ba36302419d295f1973d5fb7c5348
FelypeInvictus <felype.sales@outlook.com>
2022-12-30 14:00:30 -0300

    Adionado caminho das fotos de perfil do forum e atualizado caminho das images na classe Author

--- lib/models/forum/author_model.dart
+++ lib/models/forum/author_model.dart
@@ -10,34 +10,34 @@ class Author{
 
 final Author marcos = Author(
   name: 'Marcos Luis',
-  imageUrl: 'assets/images/author1.jpg'
+  imageUrl: 'assets/forum_profile/author1.jpg'
 );
 
 final Author joao = Author(
   name: 'João Sebastião',
-  imageUrl: 'assets/images/author2.jpg'
+  imageUrl: 'assets/forum_profile/author2.jpg'
 );
 
 final Author michel = Author(
   name: 'Michel Lopes',
-  imageUrl: 'assets/images/author3.jpg'
+  imageUrl: 'assets/forum_profile/author3.jpg'
 );
 
 final Author adriano = Author(
   name: 'Adriano Fonseca',
-  imageUrl: 'assets/images/author4.jpg'
+  imageUrl: 'assets/forum_profile/author4.jpg'
 );
 final Author junior = Author(
   name: 'Junior Alencar',
-  imageUrl: 'assets/images/author5.jpg'
+  imageUrl: 'assets/forum_profile/author5.jpg'
 );
 final Author samuel = Author(
   name: 'Samuel Rocha',
-  imageUrl: 'assets/images/author6.jpg'
+  imageUrl: 'assets/forum_profile/author6.jpg'
 );
 final Author gustavo = Author(
   name: 'Gustavo Silva',
-  imageUrl: 'assets/images/author7.jpg'
+  imageUrl: 'assets/forum_profile/author7.jpg'
 );
 
 final List<Author> authors = [
