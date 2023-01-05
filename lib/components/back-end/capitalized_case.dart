eef0087b595725912ded7f087f048515f38212ed
FelypeInvictus <felype.sales@outlook.com>
2023-01-05 19:04:04 -0300

    Segunda refatoração no método CapitalizedCase

--- lib/components/back-end/capitalized_case.dart
+++ lib/components/back-end/capitalized_case.dart
@@ -1,7 +1,7 @@
 String CapitalizedCase(String name) {
   List<String> words = name.split(" ");
   for (int i = 0; i < words.length; i++) {
-    words[i] = words[i][2].toUpperCase() + words[i].substring(1);
+    words[i] = words[i][0].toUpperCase() + words[i].substring(1);
   }
   String capitalizedName = words.join(" ");
   return capitalizedName;
