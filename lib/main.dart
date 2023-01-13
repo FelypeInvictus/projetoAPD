da4d7480be38d5835f81b14be42745ac49bdb6c6
FelypeInvictus <felype.sales@outlook.com>
2023-01-13 10:15:30 -0300

    Adicionado tela de postagem no forum

--- lib/main.dart
+++ lib/main.dart
@@ -44,3 +44,4 @@ class MeuApp extends StatelessWidget {
     );
   }
 }
+
--- lib/screens/forum/home_screen.dart
+++ lib/screens/forum/home_screen.dart
@@ -1,23 +1,95 @@
 import 'package:apd/constants.dart';
 import 'package:apd/screens/forum/widgets/body.dart';
+import 'package:apd/screens/forum/widgets/create_post.dart';
 import 'package:flutter/material.dart';
-
 
 class HomePageForum extends StatefulWidget {
   @override
   _HomePageForumState createState() => _HomePageForumState();
 }
 
-
-
 class _HomePageForumState extends State<HomePageForum> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: BodyForum(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  scrollable: true,
+                  content: Stack(
+                    clipBehavior: Clip.none,
+                    //overflow: Overflow.visible,
+                    children: <Widget>[
+                      Positioned(
+                        right: 10.0,
+                        top: 3.0,
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+
+                          child: Icon(Icons.close),
+                          //foregroundColor: Colors.grey,
+                          //backgroundColor: Colors.red,
+                        ),
+                      ),
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            SizedBox(height: 40),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: 'Titulo',
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: TextFormField(),
+                            ),
+                            SizedBox(height: 40),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                maxLines: 10,  
+                                minLines: 1,
+                                decoration: InputDecoration(
+                                  labelText: 'Texto',
+                                  contentPadding:
+                                      EdgeInsets.symmetric(vertical: 40),
+                                  //icon: Icon(Icons.message ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: ElevatedButton(
+                                child: Text("Postar"),
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    _formKey.currentState!.save();
+                                  }
+                                },
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              });
+        },
         backgroundColor: kPrimaryColor,
         child: const Icon(
           Icons.edit,
@@ -27,3 +99,4 @@ class _HomePageForumState extends State<HomePageForum> {
     );
   }
 }
+s
