1c9075ff7f851fcfb98ebfe9e019756ae2f33a05
FelypeInvictus <felype.sales@outlook.com>
2023-01-19 23:11:40 -0300

    Adicionado DataTimeField

---
+++ lib/screens/book_appointments/widgets/date_time_field.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+
+
+class BasicDataTimeField extends StatefulWidget {
+  const BasicDataTimeField({super.key});
+
+  @override
+  State<BasicDataTimeField> createState() => _BasicDataTimeFieldState();
+}
+
+class _BasicDataTimeFieldState extends State<BasicDataTimeField> {
+    final format = DateFormat("dd-MM-yyyy HH:mm");
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+          
+      Text('Data e horario (${format.pattern})'),
+      SizedBox(
+        width: 300,
+      child: DateTimeField(
+        decoration: InputDecoration(
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5.0),
+            borderSide: const BorderSide(color: Colors.green)),
+        icon: const Icon(Icons.calendar_today),
+        ),
+      
+        format: format,
+        onShowPicker: (context, currentValue) async {
+          final date = await showDatePicker(
+              context: context,
+              firstDate: DateTime(1900),
+              initialDate: currentValue ?? DateTime.now(),
+              lastDate: DateTime(2100));
+          if (date != null) {
+            final time = await showTimePicker(
+              context: context,
+              initialTime:
+                  TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+            );
+            return DateTimeField.combine(date, time);
+          } else {
+            return currentValue;
+          }
+        },
+      ),
+      ),
+    ]));
+  }
+}
