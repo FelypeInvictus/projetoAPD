8ad807819b125b5e9f93ea67021280d51f918c34
FelypeInvictus <felype.sales@outlook.com>
2023-01-20 02:32:16 -0300

    Removido textfield, reajustado tela, implementado botão e Widget Form

--- lib/screens/book_appointments/components/edit_book.dart
+++ lib/screens/book_appointments/components/edit_book.dart
@@ -1,6 +1,6 @@
 import 'package:apd/screens/book_appointments/widgets/date_choose.dart';
 import 'package:apd/screens/book_appointments/widgets/date_time_field.dart';
-import 'package:apd/screens/book_appointments/widgets/dropdown.dart';
+import 'package:apd/screens/book_appointments/widgets/dropdown_list_doctors.dart';
 import 'package:apd/screens/book_appointments/widgets/dropdown_consult_type.dart';
 import 'package:apd/screens/book_appointments/widgets/input_text_user.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +13,8 @@ class BookAppointments extends StatefulWidget {
 }
 
 class _BookAppointmentsState extends State<BookAppointments> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -21,10 +23,10 @@ class _BookAppointmentsState extends State<BookAppointments> {
         showModalBottomSheet(
           context: context,
           isScrollControlled: true,
-          elevation: 20,
+          enableDrag: true,
           backgroundColor: Colors.transparent,
           builder: (context) => Container(
-            height: MediaQuery.of(context).size.height * 0.90,
+            height: MediaQuery.of(context).size.height * 0.60,
             decoration: new BoxDecoration(
               color: Colors.white,
               borderRadius: new BorderRadius.only(
@@ -32,58 +34,65 @@ class _BookAppointmentsState extends State<BookAppointments> {
                 topRight: const Radius.circular(25.0),
               ),
             ),
-            child: Column(
-                //mainAxisAlignment: MainAxisAlignment.center,
-                //mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 30),
-                    child: Row(children: [
-                      Spacer(),
-                      InkWell(
-                        onTap: () {
-                          Navigator.of(context).pop();
-                        },
+            child: Form(
+              key: _formKey,
+              child: Column(
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  //mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 30),
+                      child: Row(children: [
+                        Spacer(),
+                        InkWell(
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
 
-                        child: Icon(Icons.close),
-                        //foregroundColor: Colors.grey,
-                        //backgroundColor: Colors.red,
-                      ),
-                    ]),
-                  ),
-                  Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
-                    child: Text(
-                      'Faça seu agendamento',
+                          child: Icon(Icons.close),
+                          //foregroundColor: Colors.grey,
+                          //backgroundColor: Colors.red,
+                        ),
+                      ]),
                     ),
-                  ),
-                  
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                      child: Text(
+                        'Faça seu agendamento',
+                      ),
+                    ),
                     Padding(
                       padding: EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
                       child: Text(
                         'Selecione o psicologo, e preencha os campos devidamente',
                         style: TextStyle(color: Colors.grey, fontSize: 12),
                       ),
-                      
                     ),
-                  
-                   //dropDownPsychologist(),
-                   
-                   SizedBox(height: 40),
-                   Expanded(
-                  child: dropDownConsultType(),
-                   ),
-                   SizedBox(height: 80),
-                   Expanded(
-                    child:inputTextUser(),
-                   ),
-                   SizedBox(height: 20),
-                   Expanded(
-                   child: BasicDataTimeField()
-                   ),
-                 
-                ]),
+                    SizedBox(height: 40),
+                    Expanded(
+                      child: dropDownConsultType(),
+                    ),
+                    SizedBox(height: 20),
+                    Expanded(
+                      child: BasicDataTimeField(),
+                    ),
+                    Expanded(
+                      child: SizedBox(
+                          width: 100,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  _formKey.currentState!.save();
+                                }
+                              },
+                              child: Text('Enviar'))),
+                    ),
+                    SizedBox(height: 10),
+
+                    
+                  ]),
+            ),
           ),
         );
       },
