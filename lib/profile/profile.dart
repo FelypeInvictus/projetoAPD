65f751fba5c07ae92a5cc5025e3835f86708cd5d
FelypeInvictus <felype.sales@outlook.com>
2023-01-23 17:09:10 -0300

    Adicionado tela de perfil/configuração

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,6 +1,10 @@
 import 'package:apd/components/capitalized_case.dart';
+import 'package:apd/components/constants.dart';
 import 'package:apd/screens/signup/components/signup_form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class profileUser extends StatefulWidget {
   const profileUser({super.key});
@@ -12,108 +16,79 @@ class profileUser extends StatefulWidget {
 class _profileUserState extends State<profileUser> {
   @override
   Widget build(BuildContext context) {
-    return ListView(children: <Widget>[
-      Center(
+    return Center(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 40),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                  height: 30,
+      padding: EdgeInsets.symmetric(horizontal: 110),
+      child: SingleChildScrollView(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Image.asset('assets/icons/iconAPD/iconAPD-removebg.png'),
+              SizedBox(height: 5),
+              InkWell(
+                child: Text('Agendamentos',
+                    style: TextStyle(
+                      fontSize: 20,
+                    )),
+              ),
+              SizedBox(height: 30),
+              InkWell(
+                child: Text('Configurações',
+                    style: TextStyle(
+                      fontSize: 20,
+                    )),
+                // SizedBox(width: 8),
+                // Icon(Icons.event),
+              ),
+              SizedBox(height: 30),
+              InkWell(
+                child: Text('Contatos',
+                    style: TextStyle(
+                      fontSize: 20,
+                    )),
+              ),
+              SizedBox(height: 30),
+              InkWell(
+                child: Text('Temas',
+                    style: TextStyle(
+                      fontSize: 20,
+                    )),
+              ),
+              SizedBox(height: 30),
+              InkWell(
+                child: Text('Sobre',
+                    style: TextStyle(
+                      fontSize: 20,
+                    )),
+              ),
+              SizedBox(height: 30),
+             
+              Ink(
+                decoration: BoxDecoration(
+                  color: Colors.white70,
+                  shape: BoxShape.rectangle,
                 ),
-                photoProfile(),
-                SizedBox(height: 8),
-                nameUser(),
-                SizedBox(height: 70),
+                child: InkWell(
+                  
+                  child: Text('Sair',
+                      style: TextStyle(
+                        fontSize: 20,
+                      )),
+                      splashFactory: InkRipple.splashFactory,
+                  onTap: () async {
 
-                     
-                // InkWell(
-
-                 
-                //   // child: Expanded(
-                //   //   child: Row(
-                //   //     //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //   //     children: <Widget>[
-                //   //       Text('Agendamentos',
-                //   //           style: TextStyle(
-                //   //             fontSize: 25,
-                //   //           )),
-                //   //       // SizedBox(width: 8),
-                //   //       // Icon(Icons.event),
-                //   //     ],
-                //   //   ),
-                //   // ),
-                // ),
-                
-
+                    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('is_logged_out', true);
+    SystemNavigator.pop();
+                    debugPrint('Deu certo');
+                    
+                  },
+                ),
+              ),
               
-
-
-
-
-
-
-
-
-
-
-
-                // SizedBox(height: 30),
-                // InkWell(
-                //   child: Expanded(
-                //     child: Row(
-                //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //       children: <Widget>[
-                //         Text('Contatos',
-                //             style: TextStyle(
-                //               fontSize: 25,
-                              
-                //             )),
-                //         // SizedBox(width: 8),
-                //         // Icon(Icons.contacts),
-                //       ],
-                //     ),
-                //   ),
-                // ),
-               
-
-
-
-
-
-
-
-
-
-
-
-
-
-              ]),
-        ),
+            ]),
       ),
-    ]);
+    ));
   }
-
-  SizedBox photoProfile() {
-    return SizedBox(
-      width: 100,
-      height: 100,
-      child: CircleAvatar(
-        backgroundImage: AssetImage('assets/profile/user/user_default.png'),
-        radius: 30,
-      ),
-    );
-  }
-}
-
-Text nameUser() {
-  return Text(
-    //Nome do usuario
-    CapitalizedCase(loginUPName, loginUPLastName),
-    style: TextStyle(
-        fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black),
-  );
 }
